e375c69e202141bd2f179603f227459d802f7394
María José Rodríguez F <rodriguez.mariajose.f@gmail.com>
2017-04-26 21:21:42 -0300

    mensaje de clase

---
+++ assets/js/main.js
@@ -0,0 +1,17 @@
+<script>
+	function mensaje(){
+		var msje = document.getElementsByClassName('mensaje');
+		  if(msje.length == 1){
+		  	 alert("Es muy poco");
+		  }
+		  else if(msje.length > 2 && msje.length < 4){
+          	alert("No está mal");
+          }else{
+          	alert("Son muchos!");
+          }
+	};
+	  mensaje();
+</script>
+
+
+
